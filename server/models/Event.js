85a9f8bd628f5e8b9253687f47cb64426a0575f7
ThakurDivyanshsingh-77 <divyanshthakur.2251@gmail.com>
2026-03-04 09:58:02 +0530

    Initial commit

--- server/models/Event.js
+++ server/models/Event.js
@@ -6,6 +6,7 @@ const eventSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   time: { type: String, required: true },
   location: { type: String, required: true },
+  image: { type: String, default: "" }, // relative path to uploaded cover image
   // ADDED THIS
   max_participants: { type: Number, default: null }, 
   type: {
